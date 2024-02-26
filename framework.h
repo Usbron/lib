55f1acdc7d3e6e680351a51eaedf683d5270c5ca
Usbron <91209224+Usbron@users.noreply.github.com>
2024-02-27 02:22:16 +0300

    Add files via upload

---
+++ framework.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // Исключите редко используемые компоненты из заголовков Windows
+#include <iostream>
+#include <vector>
+#include <string>
+
+using Lexem = std::pair<std::string, std::string>;
+
+const Lexem LEX_EMPTY = { "", "" };
+const Lexem LEX_ERROR = { "error", "" };
+const Lexem LEX_EOF = { "$", "" };
+
+void read(char& cache, std::istream& stream);
+std::pair<int, Lexem> tick(int state, std::istream& stream, char& cache);
+
